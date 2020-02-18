6599256a6f4b694d061bef90ecbe8841cdcd7de6
aaa4521 <31670356+aaa4521@users.noreply.github.com>
2020-02-18 01:18:41 -0600

    Updated API.js to save entries in and retrieve them in a sorted order

--- src/Server/API.js
+++ src/Server/API.js
@@ -1,34 +1,32 @@
-const uri = "mongodb+srv://Blogger:blogger123@blogcluster-4fqsl.gcp.mongodb.net/test?retryWrites=true&w=majority"
+const uri = "mongodb+srv://AAA2502:AMAZING1@cluster0-i2p0i.gcp.mongodb.net/test?retryWrites=true&w=majority"
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 mongoose.connect(uri, {useNewUrlParser: true});
 const db = mongoose.connection;
+var postSchema = new mongoose.Schema({
+    name: String,
+    title: String,
+    date: Date,
+    imgsrc: String,
+    body: String
+});
+var Post = mongoose.model('Post', postSchema);
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-    /*
-    // Practice with mongoose & test connection
-    var dessertPostSchema = new mongoose.Schema({
-        user: String,
-        image: String
 
-      });
-    var Kitten = mongoose.model('Kitten', kittySchema);
-    var silence = new Kitten({ name: 'Silence' });
-    console.log(silence.name); // 'Silence'
-    silence.save(function (err, silence) {
+db.once('open', function funcName(userInfo) {
+    // Practice with mongoose & test connection
+    var currentPost = new Post({ ...userInfo });
+    //console.log(userInfo); // 'Silence'
+    currentPost.save(function (err, currentPost) {
         if (err) return console.error(err);
         console.log("No error!")
     });
-    Kitten.find(function (err, kittens) {
-        if (err) return console.error(err);
-        console.log(kittens);
-      })
-    */
-   console.log("Connected!");   
+    console.log("Connected!");
 });
 
-/*
-Get request 
-Post request
-*/
+Post.find(function (err, posts) {
+    if (err) return console.error(err);
+    console.log(posts);
+    return posts;
+}).sort('-date').exec(function (err, docs) { });
