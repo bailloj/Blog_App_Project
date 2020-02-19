83675566ec098d50659bc44effffd401224c5f3e
Jireh <bailloj@utexas.edu>
2020-02-18 22:42:06 -0600

    shows the posts now

--- src/App.js
+++ src/App.js
@@ -1,70 +1,95 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col, Media, Form, Button, Jumbotron, Image,Carousel} from 'react-bootstrap';
+import { Container, Row, Col, Media, Form, Button, Jumbotron, Image,Carousel, Card, ListGroup} from 'react-bootstrap';
 import { GoogleLogin } from 'react-google-login';
-//import { getPosts } from './Server/API.js'
+import { getAllPosts, savePost } from './Server/API.js'
+const ScrollArea = require('react-scrollbar');
 
-/*<link
-  rel="stylesheet"
-  href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-  integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-  crossorigin="anonymous"
-/>*/
 
-  /*<Col sm={4}>
-          <Image src="https://community.breatheheavy.com/wp-content/uploads/2020/01/justin-bieber-yummy-charts-1024x399.jpg" thumbnail/>
-          </Col>*/
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.newSubmission = this.newSubmission.bind(this);
+    this.state = {posts:[]};
+  }
 
-function App() {
+  //Add function to change state called by <Logging> (Get request)
+  newSubmission(){
+    let allPosts = getAllPosts();
+    this.setState({posts:allPosts})
+  }
+  //Remember that parameters we pass into BlogPosts is state changed by get request
   
-  return (
-      <Container fluid>
+  render(){
+    let listItems = [];
+    if (this.state.posts.length==0){
+      listItems =  <h1>No posts yet!</h1>
+    }else{
+      let i;
+      for (i=0; i<this.state.posts.length; i++){
+        console.log(this.state.posts[i])
+        listItems.push(<Post {...this.state.posts[i]} key={i}/>)
+      }
+      console.log(listItems)
+    }
+    return (
+        <Container fluid>
+          <Row className="row justify-content-center">
+            <Col>
+              <h1 align="center" class="display-2">YUMMY</h1>
+            </Col>
+          </Row>
 
-        <Row className="row justify-content-center">
-          <Col>
-            <h1 align="center" class="display-2">YUMMY</h1>
-          </Col>
-        </Row>
+          <Row className="row justify-content-center">
+            <Col sm={7}>
+            <DessertImages/>
+            </Col>
+            <Col sm={5}>
+              <ListGroup as ="ul">
+               {listItems}
+              </ListGroup>
+            </Col>
+          </Row>
 
-        <Row className="row justify-content-center">
-          <Col sm={7}>
-          <DessertImages/>
-          </Col>
-          <Col sm={5}>
-            <h1>temp heading for posts</h1>
-          </Col>
-        </Row>
-        <Row className="row justify-content-center">
-          <Col sm={{ span: 5, offset: 7}}>
-            <Logging />
-          </Col>
-        </Row>
+          <Row className="row justify-content-center">
+            <Col sm={{ span: 5, offset: 7}}>
+              <Logging newSubmission={this.newSubmission}/>
+            </Col>
+          </Row>
 
-      </Container>
-  );
+        </Container>
+    );
+  }
 }
 
-
-
-
+//Post field and form for submit
 class Postplatform extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
-    this.handleChange = this.handleChange.bind(this);
+    this.state = {value: '', title:''};
+    this.handleChangeTitle = this.handleChangeTitle.bind(this);
+    this.handleChangeText = this.handleChangeText.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
+  handleChangeText(event) {
+    this.setState({...this.state, value: event.target.value});
+  }
+
+  handleChangeTitle(event) {
+    this.setState({...this.state, title: event.target.value});
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    console.log(this.state.value)
-    this.setState({value: ''});
-    // PUT POST REQUEST HERE
+    let dateSubmitted = new Date()
+    this.setState({value: '',title:''});
+    //Called post function
+    //savePost({title: this.state.title, name:this.props.name, date: dateSubmitted, imgsrc: this.props.imgsrc, body:this.state.value });
+    //Call another function passed from parent to make get request & update parent state
+    console.log(this.state);
+    this.props.newSubmission();
   }
 
   render() {
@@ -73,14 +98,14 @@ class Postplatform extends React.Component {
         <img
           width={110}
           height={110}
-          className="user-image"
-          src={this.props.imageUrl}
+          src={this.props.userDetails.imageUrl}
           alt="Generic placeholder"
         />
         <Media.Body>
           <Form onSubmit={this.handleSubmit}>
             <Form.Group controlId="Post">
-              <Form.Control as="textarea" rows="3" placeholder={"What's on your mind " + this.props.name +"?"}size="lg" value={this.state.value} onChange={this.handleChange}/>
+              <Form.Control size="md" type="text" placeholder={"Title"} value={this.state.title} onChange={this.handleChangeTitle}/> 
+              <Form.Control as="textarea" rows="3" placeholder={"Speak your truth " + this.props.userDetails.name}size="sm" value={this.state.value} onChange={this.handleChangeText}/>
             </Form.Group>
             <Button variant="primary" size="lg" type="submit" block>
               Post
@@ -91,8 +116,6 @@ class Postplatform extends React.Component {
     );
   }
 }
-
-
 
 
 class Logging extends React.Component {
@@ -106,7 +129,6 @@ class Logging extends React.Component {
     //Take response, pass into state 
     let {profileObj} = response
     this.setState({isLoggedIn: true, userDetails: profileObj})
-    console.log(this.state.userDetails)
   }
   handleLoginFail(response){
     console.log(response);
@@ -116,19 +138,16 @@ class Logging extends React.Component {
     let display;
     if(isLoggedIn){
       let userDetails= this.state.userDetails
-      console.log(userDetails)
-      display = <Postplatform {...userDetails}/>;
+      display = <Postplatform userDetails={userDetails} newSubmission={this.props.newSubmission}  />;
     }else {
       display = 
-      <Container>
-      <h5>say something</h5>
-      <GoogleLogin 
-      clientId="730767729970-53dlt93l0rd60st2h9hdb3hslvbnh3el.apps.googleusercontent.com"
-      buttonText="Login with Google"
-      onSuccess = {this.handleLoginClick}
-      onFailure = {this.handleLoginFail}
-      />
-      </Container>
+        <Card bg="primary">
+          <GoogleLogin 
+          clientId="730767729970-53dlt93l0rd60st2h9hdb3hslvbnh3el.apps.googleusercontent.com"
+          onSuccess = {this.handleLoginClick}
+          onFailure = {this.handleLoginFail}
+        />
+        </Card>
     }
     return(<div>
       {display}
@@ -136,6 +155,7 @@ class Logging extends React.Component {
   }
 
 }
+
 function DessertImages(props){
   return(
   <Carousel>
@@ -164,28 +184,36 @@ function DessertImages(props){
   )}
 
 
-/*
+
+
 //Need to pass in user image, post text from blogPosts
-function post(props){
-return <Media as="li">
-  <img
-    width={64}
-    height={64}
-    src = {props.userImage}
-  />
-  <Media.Body>
-    <p>
-      {props.text}
-    </p>
-  </Media.Body>
-</Media>
-}
+function Post(props){
+console.log(props)
+return(
+  <ListGroup.Item >
+    <Media as="li" key={props.key}>
+      <img
+        width={110}
+        height={110}
+        src = {props.imgsrc}
+        alt="Generic placeholder"
+      />
+      <Media.Body>
+        <h5>{props.title}</h5>
+        <h6>{props.name+props.date}</h6>
+        <p>
+          {props.body}
+        </p>
+      </Media.Body>
+    </Media>
+  </ListGroup.Item>
+)}
 
-function blogPosts(props){
-let postArrays = getPosts();
-const allBlogPosts = postArrays.map((p)=><post {...p} />)
-}
 
+
+
+
+/*
 Log 
 getPosts = () => {
   return 
